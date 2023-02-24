3e232a49ddbc753c89913f0f1b8548c4a4fde059
Zhuo Wang <zhuo.wang_1@nxp.com>
2023-02-24 16:35:49 +0800

    enable electron

---
+++ main.js
@@ -0,0 +1,27 @@
+const { app, BrowserWindow } = require('electron');
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: { nodeIntegration: true }
+    });
+
+    if (!app.isPackaged) {
+        win.loadURL('http://localhost:3000/');
+    } else {
+        win.loadFile('./build/index.html');
+    }
+};
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform != 'darwin') app.quit();
+});
